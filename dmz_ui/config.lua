c653761a2d7fa9b50b2125216c43ecd19428d8f4
Dreamz <96995622+TDreamz@users.noreply.github.com>
2022-01-20 08:26:03 +0700

    Add files via upload

---
+++ dmz_ui/config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+
+Config.InventoryImage = 'ic_inventory/html/img/items/'
+
+Config.DisplayDelay = 2 -- delay
+
+Config.CashLabel = 'S$%s'
+Config.BlackMoneyLabel = 'S$%s'
+
+Config.Weapon = true
---
+++ dmz_ui/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'DREAMZ#8508'
+
+description 'Inventory UI Add/Remove'
+
+ui_page "html/ui.html"
+
+client_scripts {
+	"client.lua",
+	"config.lua"
+}
+
+server_scripts {
+	"server.lua",
+	"config.lua"
+}
+
+files {
+	"html/ui.html",
+	"html/style.css",
+	"html/toastr.js", 
+	"html/script.js",
+}
